a4e51a6364de3913a3f81426d0b5657748628ba3
Vasrek <theo.denanassy@epitech.eu>
2017-06-24 19:21:08 +0200

    Quasi finit

--- Assets/Scripts/Enigme.cs
+++ Assets/Scripts/Enigme.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class Enigme : MonoBehaviour {
 
     public Transform canvas;
+    public Collider Yellow;
+    public Collider Green;
+    public Collider Red;
 
     void OnTriggerEnter(Collider Player)
     {
-        if (Player.gameObject.tag == "Player")
+        if (Player.gameObject.tag == "End")
         {
             Debug.Log("Question activated");
             canvas.gameObject.SetActive(true);
+        }
+        if (Player.gameObject.tag == "Red")
+        {
+            Debug.Log("Red");
+        }
+        if (Player.gameObject.tag == "Yellow")
+        {
+            Debug.Log("Yellow");
+        }
+        if (Player.gameObject.tag == "Green")
+        {
+            Debug.Log("Green");
         }
     }
 }
