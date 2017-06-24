6ff25d7361aeef571d152bd761805a8ac5cc4b09
Vasrek <theo.denanassy@epitech.eu>
2017-06-24 16:41:48 +0200

    Canvas

--- Assets/Scripts/Enigme.cs
+++ Assets/Scripts/Enigme.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Enigme : MonoBehaviour {
 
+    public Transform canvas;
+
     void OnTriggerEnter(Collider Player)
     {
         if (Player.gameObject.tag == "Player")
         {
             Debug.Log("Question activated");
+            canvas.gameObject.SetActive(true);
         }
     }
 }
