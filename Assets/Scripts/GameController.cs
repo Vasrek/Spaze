91f8fe2881e8cd2aa3d4d096cb01add6b18977b3
Vasrek <theo.denanassy@epitech.eu>
2017-06-24 14:38:06 +0200

    trigger

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Test1");
+        if (col.gameObject.tag == "Trap")
+        {
+
+            Debug.Log("Test2");
+            SceneManager.LoadScene(1);
+        }
+    }
+
+}
